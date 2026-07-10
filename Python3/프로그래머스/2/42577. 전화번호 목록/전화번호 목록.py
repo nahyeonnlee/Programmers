614e9bfdee03e8969cf7b8c221452eb368c150ef
nahyeonnlee <nahyeonnlee@gmail.com>
2026-07-11 00:19:09 +0900

    [level 2] Title: 전화번호 목록, Time: 359.00 ms, Memory: 39.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,15 @@
 def solution(phone_book):
+    # 모든 전화번호:true를 해시 맵에 저장
+    hash_map = {phone: True for phone in phone_book}
     
-    # 순서대로 정렬
-    phone_book.sort()
-    
-    # 정렬 시 접두어가 있다면 바로 뒤에 있을 것
-    for i in range(len(phone_book) - 1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            return False 
-        
+    # 각 전화번호의 접두어가 해시 맵에 존재하는지 확인
+    for phone_num in phone_book:
+        jumpto = ""
+        # 번호의 글자를 하나씩 이어 붙여 접두어를 만듦 
+        for char in phone_num[:-1]:
+            jumpto += char
+            # jumpto가 해시 맵에 들어있는지 검사
+            if jumpto in hash_map:
+                return False
+                
     return True
